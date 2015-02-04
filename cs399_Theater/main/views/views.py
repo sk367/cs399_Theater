afe60819e85e201326e2c72400a542105ed06f74
Erik Ward <erw38@nau.edu>
2015-02-04 10:01:32 -0700

    Added views and URLS, created HTML templates
    Accidently reverted, feel free to change anything

---
+++ cs399_Theater/main/views/views.py
@@ -0,0 +1,15 @@
+
+from django.shortcuts import render
+
+
+def index(request):
+	return render(request, 'index.html')
+
+def location(request):
+	return render(request, 'location.html')
+def merch(request):
+	return render(request, 'merch.html')
+def performances(request):
+	return render(request, 'performances.html')
+def tickets(request):
+	return render(request, 'tickets.html')
