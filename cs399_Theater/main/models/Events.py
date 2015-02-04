67322ea03bcaadaf2b7c659c359e79f642b4edc6
sk367 <sirbubbles42@gmail.com>
2015-02-04 11:40:00 -0700

    Make Events model

---
+++ cs399_Theater/main/models/Events.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Events(models.Model):
+  name = models.CharField(max_length=128, default="")
+  date = models.DateTimeField()
+  cost = models.DecimalField(max_digits = 3, decimal_places=2)
+  genre = models.CharField(max_length=50, default="folk")
+  description = models.TextField(default="This band plays music")
+  
+  
