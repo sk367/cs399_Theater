dd0e34160784fe10137cfcc0d8a5308dccf24287
Erik Ward <erw38@nau.edu>
2015-02-04 09:48:31 -0700

    Added views and URLS, created HTML templates
    They are currently blank but I was just testing to see if the views
    worked.

--- cs399_Theater/main/urls.py
+++ cs399_Theater/main/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from views.views import index, location, merch, performances, tickets
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'main.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', index),
+    url(r'^location', location),
+    url(r'^merch', merch),
+    url(r'^performances', performances),
+    url(r'^tickets', tickets),
 )
