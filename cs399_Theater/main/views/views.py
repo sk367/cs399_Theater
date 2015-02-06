610194f5f39152aa5917bb3d78ed58309a843272
Erik Ward <erw38@nau.edu>
2015-02-05 19:14:08 -0700

    Updated performance page with the events

--- cs399_Theater/main/views/views.py
+++ cs399_Theater/main/views/views.py
@@ -3,15 +3,12 @@ from django.shortcuts import render
 from main.models import Events
 
 def index(request):
-  soonest_event = Events.objects.order_by('date')[0]
-  context = {'soonest_event': soonest_event}
-  return render(request, 'index.html', context)
-
+  	return render(request, 'index.html')
 def location(request):
 	return render(request, 'location.html')
 def merch(request):
 	return render(request, 'merch.html')
 def performances(request):
-	return render(request, 'performances.html')
+	return render(request, 'performances.html', {'events': Events.objects.order_by('date')})
 def tickets(request):
 	return render(request, 'tickets.html')
