d956c08d1471bede4a87954f3ddceaa895b2f8cc
Erik Ward <erw38@nau.edu>
2015-02-05 22:32:37 -0700

    updated Merch HTML

--- cs399_Theater/main/admin.py
+++ cs399_Theater/main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from main.models import Events
 from main.models import Tickets
+from main.models import Merch
 
 admin.site.register(Events)
 admin.site.register(Tickets)
+admin.site.register(Merch)
--- cs399_Theater/main/views/views.py
+++ cs399_Theater/main/views/views.py
@@ -2,14 +2,15 @@
 from django.shortcuts import render
 from main.models import Events
 from main.models import Tickets
+from main.models import Merch
 
 def index(request):
   	return render(request, 'index.html')
 def location(request):
 	return render(request, 'location.html')
 def merch(request):
-	return render(request, 'merch.html')
+	return render(request, 'merch.html', {'merchandise': Merch.objects.order_by('price')})
 def performances(request):
 	return render(request, 'performances.html', {'events': Events.objects.order_by('date')})
 def tickets(request):
-  return render(request, 'tickets.html', {'tickets': Tickets.objects.order_by('date')})
+    return render(request, 'tickets.html', {'tickets': Tickets.objects.order_by('date')})
